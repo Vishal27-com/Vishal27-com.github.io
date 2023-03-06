200e29b10317fe4ea3b88dbca005b49f55c0604b
Vishal27-com <vishalgupta962148@gmail.com>
2023-03-06 19:29:14 +0530

    project updated

--- src/Pages/Home/Project/ProjectsData.js
+++ src/Pages/Home/Project/ProjectsData.js
@@ -2,6 +2,8 @@ import ssense1 from "../../../Resources/ssense1.png"
 import tictactoe1 from "../../../Resources/tictactoe1.png"
 import getHarvest1 from "../../../Resources/getHarvest1.png"
 import timer1 from "../../../Resources/timer1.png"
+import quiz from "../../../Resources/Q-Dashboard.png"
+import weather from "../../../Resources/Weather.png"
 
 export const projects=[{
     image:getHarvest1,
@@ -16,7 +18,7 @@ export const projects=[{
     ],
     stack:"MongoDB, Expressjs, Reactjs, Nodejs, Redux, Chakra-Ui.",
     code:"https://github.com/SHARUN266/savory-science-7410",
-    links:"https://get-harvest-rct201clone.netlify.app/"
+    link:"https://get-harvest-rct201clone.netlify.app/"
 },{
     image:ssense1,
     title:"SSENSE - (Cloned)",
@@ -25,23 +27,43 @@ export const projects=[{
     area:[
         "made product description page.",
         "made add to cart and add to bag.",
-    ],    stack:"HTML, CSS, Javascript.",
+    ],    
+    stack:"HTML, CSS, Javascript.",
     code:"https://github.com/RohitMBelure/Project-SSENSE",
-    links:"https://tangerine-bublanina-ddee4c.netlify.app/"
+    link:"https://tangerine-bublanina-ddee4c.netlify.app/"
 },{
     image:tictactoe1,
     title:"Tic Tac Toe",
     desc:"It is a simple tic tac toe game. It is used for entertainment.",
     type:"Individual project.",
-   stack:"HTML, CSS, Javascript, Reactjs, Chakra-UI.",
+    stack:"HTML, CSS, Javascript, Reactjs, Chakra-UI.",
     code:"https://github.com/Vishal27-com/Tic-Tac-Toe/tree/main/my-app",
-    links:"https://vishaltictactoe.netlify.app/"
-},{
+    link:"https://vishaltictactoe.netlify.app/"
+},
+{
     image:timer1,
     title:"Google Timer - (Clone)",
     desc:"It is a simple desktop timer and stopwatch clone. It is used to track time while doing any task. I have made it by using custom useTimer and useStopWatch Hook.",
     type:"Individual project.",
     stack:"HTML, CSS, Javascript, Reactjs, Chakra-UI.",
     code:"https://github.com/Vishal27-com/Google-Timer-Clone/tree/main/timer-stopwatch",
-    links:"https://googletimerclone.netlify.app/"
+    link:"https://googletimerclone.netlify.app/"
+},
+{
+    image:weather,
+    title:"Weather App",
+    desc:"It is used to know the weather of a particular city. It also does daily and hourly forecasting.",
+    type:"Individual project.",
+    stack:"HTML, CSS, Javascript, Reactjs, OpenWeatherMap Api, Context Api, Chakra-UI.",
+    code:"https://github.com/Vishal27-com/Weather-App",
+    link:"https://cloud-buddy.netlify.app/"
+},
+{
+    image:quiz,
+    title:"Quizester",
+    desc:"Quizester is a quiz app where user can play a quiz of different tech stack and get the result on the basis of his performance. In this app I have also made admin panel from where Only admin can login and handel quiz and user related things.",
+    type:"Individual project.",
+    stack:"HTML, CSS, Javascript, Reactjs, Nodejs, Expressjs, MongoDB, Redux, Context Api, Chakra-UI.",
+    code:"https://github.com/Vishal27-com/Quiz-App",
+    link:"https://quizester.netlify.app/"
 }]
